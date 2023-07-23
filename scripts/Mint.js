d55f622c1a1750f612c934268f5d8a027d1a92ae
Saksham kanwar <115371612+01saksham@users.noreply.github.com>
2023-07-24 02:11:03 +0530

    initial commit

--- scripts/Mint.js
+++ scripts/Mint.js
@@ -2,9 +2,16 @@ const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function mintNFTs() {
+  // Replace with the contract address of your AbstractArt NFT
   const nftAddress = "0x973bce79732fc1A61bA27a8117664C6d7398c1CF"; 
+
+  // Get the contract instance
   const contract = await ethers.getContractAt("AbstractArt", nftAddress);
-  const accountAddress = "0xb12F4664A25AF8b5eCe76dACCB74d2379383CB74" ; // 
+
+  // Replace with the address where you want to mint the NFTs
+  const accountAddress = "0xb12F4664A25AF8b5eCe76dACCB74d2379383CB74";
+
+  // Metadata URIs for each NFT
   const metadataURIs = [
     "https://gateway.pinata.cloud/ipfs/QmdTNAuJmYeMAbizNzTJdfEbvCtf9wN4dF8G5pGmc1koCw?filename=1.json",
     "https://gateway.pinata.cloud/ipfs/QmQoJVj5zji82msuK5WnLfRAA4aLyJAX9Fa7efAW6DiLhx?filename=2.json",
@@ -12,7 +19,8 @@ async function mintNFTs() {
     "https://gateway.pinata.cloud/ipfs/QmUpFcTQSCjk5PHjvTyQ6DZQ5cogvdELKpRqVJ8hYm35Lo?filename=4.json",
     "https://gateway.pinata.cloud/ipfs/QmTVHCUftqaW1qr4APhCmeGZxCqys21vyYo11WHWpnAt7x?filename=5.json"
   ];
-  const numNFTs = 5; 
+
+  const numNFTs = 5; // Number of NFTs to mint
 
   console.log("=== Starting NFT Minting Process ===");
 
@@ -21,7 +29,7 @@ async function mintNFTs() {
     console.log(`Minting NFT #${i + 1} with metadata URI: ${metadataURI}`);
 
     try {
-      
+      // Call the contract's mint function
       const transaction = await contract.mint(accountAddress, metadataURI);
       await transaction.wait();
 
